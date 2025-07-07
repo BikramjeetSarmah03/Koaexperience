42c8cc27315e4f2582469ad70c283f0127043574
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-07-07 23:20:52 +0530

    INIT

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,15 +1,15 @@
-import { RouterProvider, createRouter } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
 import Loader from "./components/loader";
 import { routeTree } from "./routeTree.gen";
-
 
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
   defaultPendingComponent: () => <Loader />,
   context: {},
-  });
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
