4a82ffa3b1ad09412728c9320afad99a60964f77
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-07-08 18:01:43 +0530

    formatting + structure

---
+++ apps/mobile/src/app/(drawer)/index.tsx
@@ -0,0 +1,76 @@
+import { authClient } from "@/lib/auth-client";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+
+import { Container } from "@/components/container";
+import { SignIn } from "@/components/sign-in";
+import { SignUp } from "@/components/sign-up";
+import { apiClient } from "@/lib/api-client";
+
+export default function Home() {
+  const { data: session } = authClient.useSession();
+  const health = apiClient.health.$get();
+
+  return (
+    <Container>
+      <ScrollView className="flex-1">
+        <View className="px-4">
+          <Text className="mb-4 font-mono font-bold text-foreground text-3xl">
+            BETTER T STACK
+          </Text>
+          {session?.user ? (
+            <View className="bg-card mb-6 p-4 border border-border rounded-lg">
+              <View className="flex-row justify-between items-center mb-2">
+                <Text className="text-foreground text-base">
+                  Welcome,{" "}
+                  <Text className="font-medium">{session.user.name}</Text>
+                </Text>
+              </View>
+              <Text className="mb-4 text-muted-foreground text-sm">
+                {session.user.email}
+              </Text>
+
+              <TouchableOpacity
+                className="self-start bg-destructive px-4 py-2 rounded-md"
+                onPress={() => {
+                  authClient.signOut();
+                  //   queryClient.invalidateQueries();
+                }}
+              >
+                <Text className="font-medium text-white">Sign Out</Text>
+              </TouchableOpacity>
+            </View>
+          ) : null}
+          <View className="mb-6 p-4 border border-border rounded-lg">
+            <Text className="mb-3 font-medium text-foreground">API Status</Text>
+            <View className="flex-row items-center gap-2">
+              <View className={"h-3 w-3 rounded-full"} />
+              <Text className="text-muted-foreground">
+                {/* {health
+                  .then(async (data) => {
+                    const res = await data.json();
+
+                    return res.message;
+                  })
+                  .catch(() => {
+                    return "NOT OK";
+                  })} */}
+              </Text>
+            </View>
+          </View>
+          <View className="mb-6 p-4 border border-border rounded-lg">
+            <Text className="mb-3 font-medium text-foreground">
+              Private Data
+            </Text>
+            Private
+          </View>
+          {!session?.user && (
+            <>
+              <SignIn />
+              <SignUp />
+            </>
+          )}
+        </View>
+      </ScrollView>
+    </Container>
+  );
+}
--- apps/mobile/src/lib/auth-client.ts
+++ apps/mobile/src/lib/auth-client.ts
@@ -3,11 +3,11 @@ import { createAuthClient } from "better-auth/react";
 import * as SecureStore from "expo-secure-store";
 
 export const authClient = createAuthClient({
-	baseURL: process.env.EXPO_PUBLIC_SERVER_URL,
-	plugins: [
-		expoClient({
-			storagePrefix: "eventure",
-			storage: SecureStore,
-		}),
-	],
+  baseURL: process.env.EXPO_PUBLIC_SERVER_URL,
+  plugins: [
+    expoClient({
+      storagePrefix: "koaexperience",
+      storage: SecureStore,
+    }),
+  ],
 });
